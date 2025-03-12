5b7acca8072dc2bdcc6af70fa40cb453684029b5
世林 温 <sl-wen@outlook.com>
2025-03-12 21:36:18 +0800

    firebase

---
+++ static/js/firebase-config.js
@@ -0,0 +1,22 @@
+// Firebase 配置
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuXDfNvLwiISoMwzcUIwUbaPTl69uRnao",
+  authDomain: "slwen-45838.firebaseapp.com",
+  projectId: "slwen-45838",
+  storageBucket: "slwen-45838.firebasestorage.app",
+  messagingSenderId: "734137620659",
+  appId: "1:734137620659:web:81ce8b971dce766d67b8c6",
+  measurementId: "G-WEBZLW3S59"
+};
+
+// 初始化 Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth }; 

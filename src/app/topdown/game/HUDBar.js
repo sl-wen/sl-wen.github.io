1537b57fa09b70346873bb5f2ad17a74d46113a5
sl-wen <sl-wen@outlook.com>
2025-09-05 10:50:05 +0900

    20

--- src/app/topdown/game/HUDBar.js
+++ src/app/topdown/game/HUDBar.js
@@ -11,7 +11,7 @@ const Bar = styled('div')(({ multiplier }) => ({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  padding: `0 ${12 * multiplier}px`,
+  padding: `0 ${4 * multiplier}px`,
   pointerEvents: 'none',
   zIndex: 1001,
 }));
@@ -20,26 +20,14 @@ const PixelButton = styled('button')(({ multiplier }) => ({
   imageRendering: 'pixelated',
   fontFamily: '"Press Start 2P"',
   fontSize: `${8 * multiplier}px`,
-  backgroundColor: '#e2b27e',
   border: 'solid',
   borderImage: `url("/game/assets/images/dialog_borderbox.png") 6 / ${6 * multiplier}px ${6 * multiplier}px ${6 * multiplier}px ${6 * multiplier}px stretch`,
-  padding: `${2 * multiplier}px ${3 * multiplier}px`,
+  padding: `${2 * multiplier}px ${2 * multiplier}px`,
   color: '#1b0f0a',
   display: 'flex',
   alignItems: 'center',
-  gap: `${6 * multiplier}px`,
   cursor: 'pointer',
   pointerEvents: 'auto',
-}));
-
-const AvatarImg = styled('div')(({ src, multiplier }) => ({
-  width: `${24 * multiplier}px`,
-  height: `${24 * multiplier}px`,
-  backgroundColor: '#cfa67e',
-  border: '1px solid #79584f',
-  backgroundImage: src ? `url(${src})` : 'none',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
 }));
 
 const HUDBar = ({ gameSize, avatarUrl, onAvatarClick, onSettingsClick }) => {
@@ -47,7 +35,6 @@ const HUDBar = ({ gameSize, avatarUrl, onAvatarClick, onSettingsClick }) => {
   return (
     <Bar multiplier={multiplier}>
       <PixelButton multiplier={multiplier} onClick={onAvatarClick}>
-        <AvatarImg multiplier={multiplier} src={avatarUrl} />
         👜
       </PixelButton>
       <PixelButton multiplier={multiplier} onClick={onSettingsClick}>

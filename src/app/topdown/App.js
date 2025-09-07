74ff33330a86ab89e41ac36279623ebbeebdb979
世林 温 <sl-wen@outlook.com>
2025-09-07 18:33:15 +0800

    28

--- src/app/topdown/App.js
+++ src/app/topdown/App.js
@@ -279,6 +279,9 @@ function App() {
       ],
       physics: {
         default: 'arcade',                   // 物理引擎：街机物理
+        arcade: {
+          debug: true,                       // 启用物理调试模式
+        },
       },
       plugins: {
         scene: [
